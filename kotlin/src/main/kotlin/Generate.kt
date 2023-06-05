db1df77ec20e89eebc07ba3f93a7269e66f74a34
Svetlana Novikova <svetlana.novikova@jetbrains.com>
2023-06-05 21:55:27 +0200

    Remove some unneeded elements

--- kotlin/src/main/kotlin/Generate.kt
+++ kotlin/src/main/kotlin/Generate.kt
@@ -4,10 +4,11 @@ import dsl.List
 val paths = parse("/Users/svetlana.novikova_1/IdeaProjects/Kotlin-DSL-API/kotlin/space-open-api.json")
 
 val content = StardustDslContentProvider {
+
     paths.forEach { path ->
         chapter(title = path.url) {
             path.methods.forEach { method ->
-                val font = when(method.type){
+                val font = when (method.type) {
                     "get" -> "Blue"
                     "post" -> "Green"
                     "delete" -> "Red"
@@ -15,40 +16,31 @@ val content = StardustDslContentProvider {
                     "patch" -> "Orange"
                     else -> throw RuntimeException("Invalid method type: ${method.type}")
                 }
-                xml ("""<p>Method type: <b><font color="$font">${method.type.uppercase()}</font></b></p><deflist><def title="${method.summary}"><p>${method.description}</p></def></deflist>""")
-//                p {+ method.summary}
-//                p {+ method.description}
+                xml("""
+                     <p>Method type: <b><font color="$font">${method.type.uppercase()}</font></b></p>
+                     <deflist>
+                         <def title="${method.summary}">
+                             <p>${method.description}</p>
+                         </def>
+                     </deflist>""".trimIndent())
+
                 links[path.url]?.let { xml("""<p>For more information refer to: <a href="$it"/></p>""") }
             }
             table {
                 tr {
-                    td {
-                        + "Parameter name"
-                    }
-                    td {
-                        + "Parameter type"
-                    }
+                    td { +"Parameter name" }; td { +"Parameter type" }; td { +"Required" }
                 }
                 path.methods.forEach { method ->
                     method.parameters.forEach { parameter ->
                         tr {
-                            td {
-                                + parameter.name
-                            }
-                            td {
-                                + parameter.type
-                            }
+                            td { +parameter.name }; td { +parameter.type }; td { + if (parameter.required) "Yes" else "No" }
                         }
                     }
                 }
             }
         }
     }
-    this
-}
-
-private fun List.listItemN(n: Int) {
-    li { +"List item $n" }
+        this
 }
 
 fun main() {
@@ -61,5 +53,6 @@ fun main() {
 
 val links = mapOf(
     "/absences" to "https://www.jetbrains.com/help/space/configure-absences.html",
+    "/projects" to "https://www.jetbrains.com/help/space/projects.html",
 
     )
